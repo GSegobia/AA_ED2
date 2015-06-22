cb5cd34856e75d4ee50f9cb30503b61f233a5e77
GSegobia <gabriel_segobia@hotmail.com>
2015-06-21 21:06:13 -0300

    Arvore B

--- src/arvore/ArvoreB.java
+++ src/arvore/ArvoreB.java
@@ -1,5 +1,21 @@
 package arvore;
 
 public class ArvoreB {
-
+	
+	private Pagina raiz;
+	private int altura;
+	private int numPgs;
+	private int ordem;
+	
+	public ArvoreB(int ordem){
+		
+		this.ordem = ordem;
+		this.altura = 1;
+		this.numPgs = 1;
+	}
+	
+	public void inicializa(int valor){
+		
+		
+	}
 }
--- src/arvore/Pagina.java
+++ src/arvore/Pagina.java
@@ -2,5 +2,33 @@ package arvore;
 
 public class Pagina {
 	
+	private int numChaves;
 	private Pagina[] filhos;
+	private int[] chaves;
+	boolean folha;
+	
+	public Pagina(int ordem, int valor){
+		
+		numChaves = 0;
+		filhos = new Pagina[2 * ordem + 1]; //Raiz(2---2D+1) | Páginas(D---2D+1) 
+		chaves = new int[2 * ordem];//Raiz(1---2D) | Páginas(D---2D)
+		
+		
+	}
+	
+	private boolean verificaChaves(){
+		
+		if(chaves[chaves.length - 1] != null)
+			return true;
+		else
+			return false;
+	}
+	
+	private boolean verificaFilhos(){
+		
+		if(filhos[filhos.length - 1] != null)
+			return true;
+		else
+			return false;
+	}
 }
